bad7f6d0745de0b6a314a46f2fedfc045a050f5a
feyisara.odukoya <feyisara.odukoya@flutterwavego.com>
2023-08-01 14:44:31 +0100

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Feyisara Odukoya on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
